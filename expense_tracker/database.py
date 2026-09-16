2fc83c2051891cbd2e56cd95e7ba87853f9f6cd9
Oleksandr B. <smilller24@gmail.com>
2026-09-16 22:08:09 +0300

    fix

--- expense_tracker/database.py
+++ expense_tracker/database.py
@@ -14,10 +14,6 @@ def init_db():
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute("DROP TABLE IF EXISTS budgets")
-    cursor.execute("DROP TABLE IF EXISTS transactions")
-    cursor.execute("DROP TABLE IF EXISTS users")
-
     # ТАБЛИЦЯ КОРИСТУВАЧІВ
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS users(
